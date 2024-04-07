5ed40870651c66ea5cd4f996ccb5562ca0fc8ec7
QBER <test>
2024-04-07 18:04:40 +0700

    api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
-const citySelect = document.querySelector("#city");
-const cityChoices = new Choices(citySelect, {
-  searchEnabled: false,
-  itemSelectText: "",
-});
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
+getData = async (url, cbSuccess, cbError) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    cbSuccess(data);
+  } catch (err) {
+    cbError(err);
+  }
+};
+
+const init = () => {
+  const citySelect = document.querySelector("#city");
+  const cityChoices = new Choices(citySelect, {
+    searchEnabled: false,
+    itemSelectText: "",
+  });
+
+  getData(
+    `${API_URL}${LOCATION_URL}`,
+    (locationData) => {
+      const locations = locationData.map((location) => ({
+        value: location,
+      }));
+      // cityChoices.setChoices(locations,
+      cityChoices.setChoices(locations, "value", "label", false);
+    },
+    (err) => {
+      console.warn(err);
+    }
+  );
+};
+
+init();
